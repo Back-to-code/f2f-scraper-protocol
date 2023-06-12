55745d2d571ef04bbe89d43bd9cf04e16135eeba
Pieter Huizenga <mail@pieterhuizenga.com>
2023-06-12 14:05:33 +0200

    Update examples and readmes

--- examples/main.ts
+++ examples/main.ts
@@ -1,33 +1,40 @@
-import { Cv, Handlers, Server } from "../mod.ts"
-import "https://deno.land/std@0.190.0/dotenv/load.ts"
+import { Cv, Handlers, Server } from "../mod.ts";
+import "https://deno.land/std@0.190.0/dotenv/load.ts";
 
 const handlers: Handlers = {
 	checkCredentials(username, password) {
-		return username == "root" && password == "toor"
+		return username == "root" && password == "toor";
 	},
-}
+};
 
-const server = new Server(handlers, {})
-
-server.addCustomHandler([
-	{
-		method: "GET",
-		path: "/",
-		handler: (_: Request) => {
-			return new Response("Hello World")
+// Create a new server instance
+const server = new Server(handlers, {
+	// Add custom handlers
+	customHandlers: [
+		{
+			method: "GET",
+			path: "/hello",
+			handler: (_: Request) => {
+				return new Response("Hello World");
+			},
 		},
-	},
-])
+	],
+});
 
 // Start the server, this returns a promise but will not be awaited as it will basically block forever
-await server.startServer()
+server.startServer();
 
-const loginUsers = await server.getUsers(true)
-console.log("loginUsers", loginUsers)
+const loginUsers = await server.getUsers(true);
+console.log("loginUsers", loginUsers);
 
 // Start scraping here
 
 const cv: Cv = {
 	referenceNumber: "test-123",
+};
+server.sendCv(cv);
+
+// keep the server running
+while (true) {
+	await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 60 * 10));
 }
-server.sendCv(cv)
